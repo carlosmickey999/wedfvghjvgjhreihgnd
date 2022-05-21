9b644a40d0f155a9bd4d4ec6099b1fb53c7b6670
carlosmickey999 <99065993+carlosmickey999@users.noreply.github.com>
2022-05-21 15:02:20 +0530

    Add files via upload

---
+++ bot/helper/telegram_helper/bot_commands.py
@@ -0,0 +1,39 @@
+class _BotCommands:
+    def __init__(self):
+        self.StartCommand = "start"
+        self.MirrorCommand = "mirror4"
+        self.UnzipMirrorCommand = "unzipmirror4"
+        self.ZipMirrorCommand = "zipmirror4"
+        self.TarMirrorCommand = "tarmirror4"
+        self.CancelMirror = "cancel4"
+        self.CancelAllCommand = "cancelall4"
+        self.ListCommand = "list4"
+        self.StatusCommand = "status4"
+        self.AuthorizeCommand = "authorize4"
+        self.UnAuthorizeCommand = "unauthorize4"
+        self.AuthorizedUsersCommand = 'users4'
+        self.AddSudoCommand = 'addsudo4'
+        self.RmSudoCommand = 'rmsudo4'
+        self.PingCommand = "ping4"
+        self.RestartCommand = "restart4"
+        self.StatsCommand = "stats4"
+        self.HelpCommand = "help4"
+        self.LogCommand = "log4"
+        self.CloneCommand = "clone4"
+        self.WatchCommand = "watch4"
+        self.ZipWatchCommand = "zipwatch4"
+        self.TarWatchCommand = "tarwatch4"
+        self.deleteCommand = "del4"
+        self.LeechSetCommand = 'leechset4'
+        self.SetThumbCommand = 'setthumb4'
+        self.LeechCommand = 'leech4'
+        self.TarLeechCommand = 'tarleech4'
+        self.UnzipLeechCommand = 'unzipleech4'
+        self.ZipLeechCommand = 'zipleech4'
+        self.LeechWatchCommand = 'ytdl4'
+        self.LeechTarWatchCommand = 'ytdltar4'
+        self.LeechZipWatchCommand = 'ytdlzip4'
+        self.CountCommand = "count4"
+        self.SpeedCommand = "speed4"
+
+BotCommands = _BotCommands()
